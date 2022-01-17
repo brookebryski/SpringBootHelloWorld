bbdd12426a171baf4f2c81badc5ec4c410257e2a
BrookeBryski <brookeabryski@gmail.com>
2022-01-17 14:46:54 -0600

    started greeting controller and class

---
+++ src/main/java/com/example/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public void greeting(@RequestParam(value="name")String name)
+    {
+
+    }
+}
